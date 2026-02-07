d396b4eb23802f89d4824cf0a75c8bd2efae25d6
Mogeni <evansmogeni254@gmail.com>
2026-02-07 23:58:20 +0300

    add credit cards in my cards screen

--- lib/pages/my_card.dart
+++ lib/pages/my_card.dart
@@ -15,26 +15,50 @@ class MyCardPage extends StatelessWidget {
           onPressed: () {},
           icon: Icon(Icons.arrow_back_ios_new, size: 20),
         ),
-        title: Text(
-          "My Card",
-          style: TextStyle(
-            fontWeight: FontWeight.w500,
-          )
-        ),
+        title: Text("My Card", style: TextStyle(fontWeight: FontWeight.w500)),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              SizedBox(height: 20,),
+              SizedBox(height: 20),
+              // Back Card
+              BackCard(),
+              SizedBox(height: 20),
+              // Front Card
               CreditCard(),
-              SizedBox(height: 20,),
-              CreditCard()
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class BackCard extends StatelessWidget {
+  const BackCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 240,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Color.fromARGB(255, 14, 19, 29),
+      ),
+      child: Stack(children: [
+        Positioned(
+          top: 0,
+          right: 0,
+          bottom: 0,
+          child: Image.asset(
+            "assets/images/chip.png",
+            fiT
+          ),
+
+        )
+      ]),
     );
   }
 }
