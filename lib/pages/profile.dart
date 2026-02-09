8c533fe65b5e33ad5fdb9c99629683d1bcab82db
Mogeni <evansmogeni254@gmail.com>
2026-02-09 23:55:24 +0300

    profile screen

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -7,8 +7,20 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("My Profile Page"),
+        child: Profile(),
       ),
+    );
+  }
+}
+
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("My Profile Screens"),
     );
   }
 }
